1ac994a08f5f3c132abdb545fef6e7dc34ceebd2
singhveerraj <singhveerraj.rv@gmail.com>
2020-11-20 19:36:54 +0530

    c28

--- BaseClass.js
+++ BaseClass.js
@@ -5,6 +5,7 @@ class BaseClass{
             'friction':1.0,
             'density':1.0
         }
+        
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
